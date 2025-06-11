c0b629ee4d4fe2555dd212d444e46b36e8889afd
Jonas Bjørge Andersen <dev@jonasandersen.no>
2025-06-11 23:56:19 +0200

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,4 +10,4 @@ set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 set -x PATH $PATH $HOME/bin
 set -x PATH $PATH $HOME/.local/share/JetBrains/Toolbox/scripts
-set -x JAVA_HOME /usr/bin/java
+set -x JAVA_HOME /etc/alternatives/java_sdk/
